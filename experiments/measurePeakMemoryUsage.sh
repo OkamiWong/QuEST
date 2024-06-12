566152912c27bd956f020fe8b90a0a616fb3effe
Okami Wong <wtfimba@live.com>
2024-06-12 16:45:09 +0900

    feat(experiment/measurePeakMemoryUsage): specify qubits and update device id

--- experiments/measurePeakMemoryUsage.sh
+++ experiments/measurePeakMemoryUsage.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-program=/home/twang/sources/projects/QuEST/build/demo
-profiler="nvidia-smi --query-gpu=memory.used --format=csv -i 0 -lms 5"
+program="/home/twang/sources/projects/QuEST/build/demo 32"
+profiler="nvidia-smi --query-gpu=memory.used --format=csv -i 1 -lms 5"
 log_file="profileResult.log"
 
 $profiler > $log_file &
