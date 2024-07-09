d7ae8a09b8c371cebb08409b8e5efed55dc257d6
Okami Wong <wtfimba@live.com>
2024-07-09 10:50:11 +0900

    feat: measure time

--- examples/qft.c
+++ examples/qft.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
 
 #include "QuEST.h"
+
+double getWallClockTimeInSeconds() {
+  struct timeval t;
+  gettimeofday(&t, NULL);
+  return (double)t.tv_sec + (double)t.tv_usec * 1e-6;
+}
 
 int main(int argc, char **argv) {
   int numQubits;
@@ -18,7 +25,13 @@ int main(int argc, char **argv) {
 
   initZeroState(qureg);
 
+  double start = getWallClockTimeInSeconds();
+
   applyFullQFT(qureg);
+
+  double end = getWallClockTimeInSeconds();
+
+  printf("Total time used (s): %.6lf\n", end - start);
 
   printf("prob of solution |%llx> = %.8lf\n", 0, getProbAmp(qureg, 0));
   long long index = 0;
